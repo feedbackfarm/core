403e8d60690de4ab45c175d5c9e24215512ee302
Charles-Olivier Demers <charles-olivierdemers@hotmail.com>
2021-10-22 07:35:28 -0400

    Add page name

--- src/FeedbackCore.ts
+++ src/FeedbackCore.ts
@@ -1,13 +1,13 @@
 const apiRoute = 'https://api.feedback.farm/addFeedback';
 
-async function sendFeedback(projectId: string, feedback: string, identifier?: string) {
+async function sendFeedback(projectId: string, feedback: string, identifier?: string, pageName?: string) {
   return fetch(apiRoute, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ projectId, feedback, identifier }),
+    body: JSON.stringify({ projectId, feedback, identifier, pageName }),
   });
 }
 
