757c3337d175b75b8f076781b0a0a41929fe06bc
Charles-Olivier Demers <charles-olivierdemers@hotmail.com>
2021-11-11 17:01:33 -0500

    Add type param

--- src/FeedbackCore.ts
+++ src/FeedbackCore.ts
@@ -1,13 +1,20 @@
 const apiRoute = 'https://api.feedback.farm/addFeedback';
+type FeedbackType = 'BUG' | 'FEATURE' | 'OTHER';
 
-async function sendFeedback(projectId: string, feedback: string, identifier?: string, pageName?: string) {
+async function sendFeedback(
+  projectId: string,
+  feedback: string,
+  type: FeedbackType,
+  identifier?: string,
+  pageName?: string,
+) {
   return fetch(apiRoute, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ projectId, feedback, identifier, pageName }),
+    body: JSON.stringify({ projectId, feedback, type, identifier, pageName }),
   });
 }
 
